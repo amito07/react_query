666731d490b84e36eaf4734d5abb95c100067249
amito07 <lianbadhon@gmail.com>
2022-11-12 16:00:43 +0600

    Onsuccess and OnError Functionality

--- src/components/RQSuperHeros.jsx
+++ src/components/RQSuperHeros.jsx
@@ -2,32 +2,42 @@ import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 
+const fetchFunction = () => {
+	return axios.get("http://localhost:4000/superheroes");
+};
+
 const RQSuperHeros = () => {
-	const fetchFunction = () => {
-		return axios.get("http://localhost:4000/superheroes");
+	const onSuccess = (data) => {
+		console.log("Data received from server: ",data);
+	};
+	const onError = (err) => {
+		console.log("Something went wrong",err)
 	};
 	const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
 		"super-hero",
 		fetchFunction,
-    {
-      enabled:false
-    }
+		{
+			enabled: false,
+			refetchIntervalInBackground:true,
+			onSuccess,
+			onError
+		}
 	);
 
 	if (isLoading) {
 		return <h2>Loading...</h2>;
 	} else if (isError) {
 		return <h2>{error.message}</h2>;
-	}else if(isFetching){
+	} else if (isFetching) {
 		return <h2>Fetch Again..</h2>;
-  }
+	}
 	return (
 		<>
 			<h1>Super Hero List</h1>
-      <button onClick={refetch}>Refetch Data</button>
+			<button onClick={refetch}>Refetch Data</button>
 			{data?.data.map((v, i) => {
 				return (
-					<ul>
+					<ul key={i}>
 						<li key={i}>{v.name}</li>
 					</ul>
 				);
